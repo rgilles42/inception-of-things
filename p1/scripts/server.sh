b030c40e87dae089998ee681d849916d4c508191
IOT <iot@debian-iot>
2024-01-18 17:43:32 +0100

    finished p1

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,1 +1,2 @@
-curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode 644 --advertise-address=192.168.56.110 --node-ip=192.168.56.110
+cat /proc/sys/kernel/random/uuid > /vagrant/server_token
+curl -sfL https://get.k3s.io | sh -s - server --token-file /vagrant/server_token --write-kubeconfig-mode 644 --advertise-address=192.168.56.110 --node-ip=192.168.56.110
---
+++ p1/scripts/worker.sh
@@ -0,0 +1,1 @@
+curl -sfL https://get.k3s.io | sh -s - agent --server https://rgillesS:6443 --token-file /vagrant/server_token --node-ip=192.168.56.111
