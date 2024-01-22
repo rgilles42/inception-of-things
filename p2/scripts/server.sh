dc3e64f96aad34fd9e5b8f81650667f946d65339
IOT <iot@debian-iot>
2024-01-22 17:04:48 +0100

    finished p2

---
+++ p2/scripts/server.sh
@@ -0,0 +1,4 @@
+cat /proc/sys/kernel/random/uuid > /vagrant/server_token
+curl -sfL https://get.k3s.io | sh -s - server --token-file /vagrant/server_token --write-kubeconfig-mode 644 --advertise-address=192.168.56.110 --node-ip=192.168.56.110
+sleep 10
+kubectl apply -f /vagrant/confs/deployment.yaml
