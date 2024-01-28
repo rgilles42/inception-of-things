4e1bcd54573a50db7fd3b5cfc78f5d7c83e43822
Raphael GILLES <rgilles@student.42.fr>
2024-01-28 23:37:18 +0100

    p2 provisionning now waits for pods to be ready

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode 644 --advertise-address=192.168.56.110 --node-ip=192.168.56.110
-sleep 10
+sleep 8
 kubectl apply -f /vagrant/confs/deployment.yaml
+sleep 20
 kubectl wait --for=condition=Ready pods --all
